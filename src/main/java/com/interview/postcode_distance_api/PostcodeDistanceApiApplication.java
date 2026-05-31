61bc80a33214f7b33fe0f05c77f2be222325b8f6
Wen Huan <wenhuan98@hotmail.com>
2026-05-31 15:47:12 +0800

    feat: initial commit

---
+++ src/main/java/com/interview/postcode_distance_api/PostcodeDistanceApiApplication.java
@@ -0,0 +1,13 @@
+package com.interview.postcode_distance_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostcodeDistanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostcodeDistanceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interview/postcode_distance_api/PostcodeDistanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.postcode_distance_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostcodeDistanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
